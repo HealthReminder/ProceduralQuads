4fb89089a0464655524d4958c3bda8391fcbe303
Eduardo-PC <43762379+HealthReminder@users.noreply.github.com>
2023-04-09 02:32:29 -0700

    More Spinning

--- ProceduralQuads/Assets/Scripts/Behaviours/RotateAroundAxis.cs
+++ ProceduralQuads/Assets/Scripts/Behaviours/RotateAroundAxis.cs
@@ -2,12 +2,24 @@ using UnityEngine;
 
 public class RotateAroundAxis : MonoBehaviour
 {
-    public Vector3 rotationAxis = Vector3.up; // the axis to rotate around
-    public float rotationSpeed = 10f; // the speed of rotation
-    
-    public float direction; //If it should rotate the other way (-1,1)
+    public bool IsRotating
+    {
+        get { return _isRotating; }
+        set => IsRotating = value;
+    }
+    [SerializeField] private bool _isRotating;
+    [SerializeField] private Vector3 _rotationAxis = Vector3.up; // the axis to rotate around
+    [SerializeField] private float _rotationSpeed = 10f; // the speed of rotation
+
+    [Range(-1, 1)] [SerializeField] private float _direction; //If it should rotate the other way (-1,1)
+    private void Update()
+    {
+        if (!_isRotating)
+            return;
+        Rotate();
+    }
     public void Rotate()
     {
-        transform.Rotate(rotationAxis, rotationSpeed * direction * Time.deltaTime);
+        transform.Rotate(_rotationAxis, _rotationSpeed * _direction * Time.deltaTime);
     }
 }
--- ProceduralQuads/Assets/Scripts/Behaviours/ScaleChangeSmooth.cs
+++ ProceduralQuads/Assets/Scripts/Behaviours/ScaleChangeSmooth.cs
@@ -7,6 +7,7 @@ public class ScaleChangeSmooth : MonoBehaviour
     public bool IsStartInvisible = true;
     public bool IsInverse { get; set; }
     public bool IsChanging { get; set; }
+    [SerializeField] private float _scaleMultiplier = 1; // Multiplies the animation curve
     [SerializeField] private AnimationCurve _scaleCurve; // Curve dictates the scale variation
     [SerializeField] private float _speed; // Movement progress, where it is in the movement
     float progress = 0; // Movement progress, where it is in the movement
@@ -27,7 +28,7 @@ public class ScaleChangeSmooth : MonoBehaviour
 
         progress += Time.deltaTime * _speed;
         Mathf.Clamp01(progress);
-        t = _scaleCurve.Evaluate(progress);
+        t = _scaleCurve.Evaluate(progress)* _scaleMultiplier;
 
         // Change scale accordingly
         transform.localScale = new Vector3(t, t, t);
