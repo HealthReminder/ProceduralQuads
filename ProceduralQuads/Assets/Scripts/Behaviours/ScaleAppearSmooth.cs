e185c92ddbd7f32ad17b142a8ffd614dc2dce9ba
Eduardo-PC <43762379+HealthReminder@users.noreply.github.com>
2023-04-13 00:35:58 -0700

    Minor Changes
    Minor aesthetics changes.

--- ProceduralQuads/Assets/Scripts/Behaviours/ScaleAppearSmooth.cs
+++ ProceduralQuads/Assets/Scripts/Behaviours/ScaleAppearSmooth.cs
@@ -7,12 +7,14 @@ public class ScaleAppearSmooth : MonoBehaviour
     public bool IsStartInvisible = true;
     public bool IsInverse { get; set; }
     public bool IsChanging { get; set; }
-    [SerializeField] private Vector3 _targetScale = Vector3.one; // Multiplies the animation curve
+     private Vector3 _targetScale = Vector3.one; // Multiplies the animation curve
     [SerializeField] private AnimationCurve _scaleCurve; // Curve dictates the scale variation
     [SerializeField] private float _speed; // Movement progress, where it is in the movement
     float progress = 0; // Movement progress, where it is in the movement
     private void Start()
     {
+        _targetScale = transform.localScale;
+
         if (IsStartInvisible)
             transform.localScale = Vector3.zero;
     }
