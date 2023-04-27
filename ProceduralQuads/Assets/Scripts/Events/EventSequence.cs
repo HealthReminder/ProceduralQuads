3725989e1938b95151535d424b0a5c2d55f0bd16
Eduardo-PC <43762379+HealthReminder@users.noreply.github.com>
2023-04-27 01:36:43 -0700

    Minor Changes
    Minor event changes in the Fourth Act.

--- ProceduralQuads/Assets/Scripts/Events/EventSequence.cs
+++ ProceduralQuads/Assets/Scripts/Events/EventSequence.cs
@@ -9,6 +9,7 @@ public class EventSequence : MonoBehaviour
     public bool IsReady { set => _isReady = value; }
     [SerializeField] private bool _isReady = true;   // If true ini
     [SerializeField] private bool _hasTriggered = false;   // If true initiate the event cascade
+    [SerializeField] private float _timeMultiplier = 1.0f; // Multiplies the time waited between events
     [System.Serializable]   public struct TimedEvent
     {
         public float DelayBefore;
@@ -30,9 +31,9 @@ public class EventSequence : MonoBehaviour
         for (int i = 0; i < _timedEvents.Length; i++)
         {
             TimedEvent e = _timedEvents[i];
-            yield return new WaitForSeconds(e.DelayBefore);
+            yield return new WaitForSeconds(e.DelayBefore* _timeMultiplier);
             e.Events.Invoke();
-            yield return new WaitForSeconds(e.DelayAfter);
+            yield return new WaitForSeconds(e.DelayAfter * _timeMultiplier);
 
 
         }
--- ProceduralQuads/Assets/Scripts/Events/OnCollisionEvent.cs
+++ ProceduralQuads/Assets/Scripts/Events/OnCollisionEvent.cs
@@ -8,6 +8,10 @@ public class ImpactEvent : UnityEvent<(float, Vector3)>
 }
 public class OnCollisionEvent : MonoBehaviour
 {
+    [Tooltip("Tag that triggers collision. Empty for everything")]
+    public string Tag;
+    [Tooltip("Check if collider can only be triggered once")]
+    public bool IsTriggeredOnce = false;
     [Tooltip("Minimum impact magnitude to trigger events")]
     public float MinimumImpactMagnitude = 2;
     [Header("Regular Events")]
@@ -16,15 +20,19 @@ public class OnCollisionEvent : MonoBehaviour
     [Tooltip("Events that pass the position and magnitude of collision an as a parameter")]
     public ImpactEvent ImpactEvents;
 
+    bool hasTriggered = false;
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.relativeVelocity.magnitude > MinimumImpactMagnitude)
         {
-            //Debug.Log(collision.relativeVelocity.magnitude);
-            Events.Invoke();
-            ImpactEvents.Invoke((collision.relativeVelocity.magnitude,collision.GetContact(0).point));
-
-
+            if(!IsTriggeredOnce || (IsTriggeredOnce && !hasTriggered))
+            if (tag == string.Empty || (tag != string.Empty && collision.transform.tag == Tag))
+            {
+                Events.Invoke();
+                ImpactEvents.Invoke((collision.relativeVelocity.magnitude, collision.GetContact(0).point));
+                    hasTriggered = true;
+            }
         }
     }
 }
