998c55c555331efcac887fd2e024d0ecb8719988
Eduardo-PC <43762379+HealthReminder@users.noreply.github.com>
2023-04-30 23:14:41 -0700

    Fourth Act Block Sequence
    Implemented the block sequence in the fourth act and changed the EventSequence script to allow other events to be sequentially called through Unity Events.

--- ProceduralQuads/Assets/Scripts/Events/EventSequence.cs
+++ ProceduralQuads/Assets/Scripts/Events/EventSequence.cs
@@ -1,17 +1,20 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class EventSequence : MonoBehaviour
 {
 
-    public bool IsReady { set => _isReady = value; }
-    [SerializeField] private bool _isReady = true;   // If true ini
+    public bool IsReady { get => _isReady; set => _isReady = value; }
+    public float TimeMultiplier { get => _timeMultiplier; set => _timeMultiplier = value; }
+    public bool HasTriggered { get => _hasTriggered; set => _hasTriggered = value; }
+    [SerializeField] private bool _isReady = true;   // If true iniates event sequence
     [SerializeField] private bool _hasTriggered = false;   // If true initiate the event cascade
     [SerializeField] private float _timeMultiplier = 1.0f; // Multiplies the time waited between events
+    int currentEvent = 0;
     [System.Serializable]   public struct TimedEvent
     {
+        public string Name;
         public float DelayBefore;
         public UnityEvent Events;
         public float DelayAfter;
@@ -23,19 +26,30 @@ public class EventSequence : MonoBehaviour
         {
             _isReady = false;
             _hasTriggered = true;
-            StartCoroutine(SequenceRoutine());
+            StartCoroutine(AllSequenceRoutine());
         }
     }
-    IEnumerator SequenceRoutine()
+    public void NextEvent()
     {
-        for (int i = 0; i < _timedEvents.Length; i++)
+        StartCoroutine(SequenceRoutine(_timedEvents[currentEvent]));
+    }
+    IEnumerator SequenceRoutine(TimedEvent e)
+    {
+        currentEvent++;
+        if (currentEvent >= _timedEvents.Length)
+            yield break;
+
+        yield return new WaitForSeconds(e.DelayBefore * _timeMultiplier);
+        e.Events.Invoke();
+        Debug.Log(e.Name + " event triggered by " + gameObject.name);
+        yield return new WaitForSeconds(e.DelayAfter * _timeMultiplier);
+    }
+    IEnumerator AllSequenceRoutine()
+    {
+        for (int i = currentEvent; i < _timedEvents.Length; i++)
         {
             TimedEvent e = _timedEvents[i];
-            yield return new WaitForSeconds(e.DelayBefore* _timeMultiplier);
-            e.Events.Invoke();
-            yield return new WaitForSeconds(e.DelayAfter * _timeMultiplier);
-
-
+            yield return SequenceRoutine(e);
         }
         yield break;
     }
